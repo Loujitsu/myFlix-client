f226e92b0a2fdb42226bdcf12fbd1584b09f00c4
Luis O <zenodro1588@gmail.com>
2022-09-15 10:17:38 -0700

    added to main-view

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -3,6 +3,7 @@ import axios from 'axios';
 
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
+import { LoginView } from '../login-view/loginin-view';
 
 
 export class MainView extends React.Component {
@@ -15,9 +16,29 @@ export class MainView extends React.Component {
         }    
     }
 
-    componentDidMount() {
-        axios.get('https://movie-flix-289893.herokuapp.com/movies')
+    onLoggedIn(Username, Password) {
+        console.log('Username: ' + Username + ' Password: ' + Password)
+        axios.post('https://movie-flix-289893.herokuapp.com/login', 
+        {
+            Username,
+            Password
+        }).then(function (response) { 
+            localStorage.setItem('token', response.data.token);
+        })
+        .catch(error => {
+            console.log(error);
+        });
+
+    }
+    componentWillMount() {
+        axios.get('https://movie-flix-289893.herokuapp.com/movies', 
+        {
+            headers : {
+                Authorization: "Bearer " + localStorage.getItem('token')             
+            }
+        })
             .then(response => {
+                console.log(response.data);
                 this.setState({
                     movies: response.data
                 });
@@ -34,19 +55,25 @@ export class MainView extends React.Component {
       }
 
     render() {
-        const { movies, selectedMovie } = this.state;
+        const { movies, selectedMovie, user } = this.state;
+        console.log(movies)
 
-        if (movies.length === 0) return <div className="main-view" />;
+    /* If there is no user, the LoginView is rendered. If there is a user logged in, the user details are *passed as a prop to the LoginView*/
+    if (!user) return <LoginView onLoggedIn={(user, pass) => this.onLoggedIn(user, pass)} />;
 
-        return (
-            <div className="main-view">
-                {selectedMovie
-                    ? <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }}/>
-                    : movies.map(movie => (
-                        <MovieCard key={movie._id} movie={movie} onMovieClick={(movie) => { this.setSelectedMovie(movie) }}/>
-            ))
+    // Before the movies have been loaded
+    if (movies.length === 0) return <div className="main-view" />;
+
+    return (
+      <div className="main-view">
+        {/*If the state of `selectedMovie` is not null, that selected movie will be returned otherwise, all *movies will be returned*/}
+        {selectedMovie
+          ? <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }}/>
+          : movies.map(movie => (
+            <MovieCard key={movie._id} movie={movie} onMovieClick={(newSelectedMovie) => { this.setSelectedMovie(newSelectedMovie) }}/>
+         ))
         }
-            </div>
+      </div>
         );
     }
 }
