72fd24a685ee1e612d1850ef9a1fcd6a329db59a
Luis O <zenodro1588@gmail.com>
2022-10-07 21:33:48 -0700

    added code

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -15,16 +15,16 @@ export class MainView extends React.Component {
         }    
     }
 
-    componentDidMount() {
-        axios.get('https://movie-flix-289893.herokuapp.com/movies', 
+    componentDidMount(){
+      axios.get('https://movie-flix-289893.herokuapp.com/movies')
         .then(response => {
-            this.setState({
-              movies: response.data
-            });
-          })
-          .catch(error => {
-            console.log(error);
+          this.setState({
+            movies: response.data
           });
+        })
+        .catch(error => {
+          console.log(error);
+        });
     }
 
     setSelectedMovie(newSelectedMovie) {
