f7e5770b76cf74e0c50d2dd387aaf337e7a65f20
Luis O <zenodro1588@gmail.com>
2022-10-22 21:52:46 -0700

    Update login-view.jsx

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -2,16 +2,28 @@ import React, { useState } from 'react';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import { FormGroup } from 'react-bootstrap';
+import axios from 'axios';
+
 
 export function LoginView(props) {
   const [ username, setUsername ] = useState('');
   const [ password, setPassword ] = useState('');
 
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(username, password);
     /* Send a request to the server for authentication */
-    props.onLoggedIn(username);
+    axios.post('movie-flix-289893.herokuapp.com/login', {
+      Username: username,
+      Password: password
+    })
+    .then(response => {
+      const data = response.data;
+      props.onLoggedIn(data);
+    })
+    .catch(e => {
+      console.log('no such user')
+    });
   };
 
   return (
@@ -29,5 +41,5 @@ export function LoginView(props) {
         Submit
       </Button>
     </Form>
-  );
+  )
 }
