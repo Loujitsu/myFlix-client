6575aca5920a228a43b9c3c45c2147a741011233
Luis O <zenodro1588@gmail.com>
2022-10-28 13:11:30 -0700

    Update login-view.jsx

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -13,7 +13,7 @@ export function LoginView(props) {
   const handleSubmit = (e) => {
     e.preventDefault();
     /* Send a request to the server for authentication */
-    axios.post('movie-flix-289893.herokuapp.com/login', {
+    axios.post('https://movie-flix-289893.herokuapp.com/login', {
       Username: username,
       Password: password
     })
